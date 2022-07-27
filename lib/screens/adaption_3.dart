d408bd59c4c2fcac3ae43bf94b85fd3c31fa2022
abdelrahman-abdelghany1 <104377203+abdelrahman-abdelghany1@users.noreply.github.com>
2022-07-27 22:14:58 +0200

    commit

--- lib/screens/adaption_3.dart
+++ lib/screens/adaption_3.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:petology/themes/colors.dart';
 import 'package:petology/widgets/footer.dart';
 import 'package:petology/widgets/navigation_bar.dart';
-//sssss
+
 class Adaption3Screen extends StatelessWidget {
-  const Adaption3Screen({Key? key}) : super(key: key);
+   Adaption3Screen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,92 +14,91 @@ class Adaption3Screen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              const WebNavigationBar(),
-              const SizedBox(
+               WebNavigationBar(),
+               SizedBox(
                 height: 100,
               ),
               Stack(
-                alignment: Alignment.center,
                 children: [
-                  Image.asset('assets/images/photo.png',),
+                  Image.asset(
+                    'assets/images/photo.png',
+                  ),
                   ListView.separated(
+                    shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
                   itemBuilder: (context, index) => buildCatItem(),
-                  separatorBuilder: (context, index) => const SizedBox(width: 20,),
+                  separatorBuilder: (context, index) =>  SizedBox(width: 20,),
                   itemCount:3,
-                  ),/////
+                  ),
                 ],
               ),
-              const Footer(),
+               Footer(),
             ],
           ),
         ),
       ),
     );
   }
-  Widget buildCatItem () =>  Container(
-    width: double.infinity,
-    height: 1450,
-    child: Column(
-      children: [
-        Container(
-          width: 481,
-          height: 760,
-          decoration: BoxDecoration(
-            color: const Color(0xFFEAEAEA),
-            borderRadius: BorderRadius.circular(88),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/dog2.png',
-                width: 380,
-                height: 380,
+  Widget buildCatItem ()=>  Column(
+    children: [
+      Container(
+        width: 481,
+        height: 760,
+        decoration: BoxDecoration(
+          color:  Color(0xFFEAEAEA),
+          borderRadius: BorderRadius.circular(88),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/dog2.png',
+              width: 380,
+              height: 380,
+            ),
+             Text(
+              'Roy',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 50,
+                color: AppColors.darkBrown,
               ),
-              const Text(
-                'Roy',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 50,
-                  color: AppColors.darkBrown,
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              MaterialButton(
-                color: const Color(0xFF492F24),
-                onPressed: () {},
-                minWidth: 355,
-                height: 109,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(80.0)),
+            ),
+             SizedBox(
+              height: 30,
+            ),
+            MaterialButton(
+              color:  Color(0xFF492F24),
+              onPressed: () {},
+              minWidth: 355,
+              height: 109,
+              shape:  RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(80.0)),
 
-                ),
-                child:  const Text(
-                  'Read More',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                    color: AppColors.lightBrown,
-                  ),
-                ),
               ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              const Text(
-                'by Sara',
+              child:   Text(
+                'Read More',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color:Color(0xFF180701),
+                  fontSize: 30,
+                  color: AppColors.lightBrown,
                 ),
               ),
-            ],
-          ),
-        )
-      ],
-    ),
+            ),
+             SizedBox(
+              height: 20.0,
+            ),
+             Text(
+              'by Sara',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color:Color(0xFF180701),
+              ),
+            ),
+          ],
+        ),
+      )
+    ],
   );
 }
